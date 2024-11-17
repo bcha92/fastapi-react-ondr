f6c3b340c8e787cbb74f9028236ece0cf19b45dc
bcha92 <87155977+bcha92@users.noreply.github.com>
2024-11-17 02:22:50 -0500

    restructured endpoints

--- backend/helper.py
+++ backend/helper.py
@@ -1,69 +1,83 @@
 from sqlmodel import SQLModel, create_engine, Session, select # type: ignore
-from model import Application
+from model import Application, AppForm
 from main import HTTPException
 from sample_data import db_name, db_sample_values
 
 # HTTP Exception Error Handling
-def httpCode(status_code: int = 404, detail: str = "Your request has not been found or does not exist."):
+def httpError(detail: str, status_code: int = 404):
     raise HTTPException(status_code, detail)
 
 class Query:
-    # DB Functions
-    def __init__(self, db_name: str = db_name):
-        self.db_name = db_name
-        global sqlite_url
-        sqlite_url = f"sqlite:///{db_name}"
-        global engine
-        engine = create_engine(sqlite_url)
+    # DB Initialization Functions
+    global sqlite_url
+    global engine
+    sqlite_url = f"sqlite:///{db_name}"
+    engine = create_engine(sqlite_url)
     
     # Helper Functions
     def get_all_applications(self) -> list[Application]:
-        all_applications = list()
-        
         with Session(engine) as session:
             statement = select(Application)
-            results = session.exec(statement)
-            for applicant in results:
-                all_applications.append(applicant)
-        
-        return all_applications
+            result = session.exec(statement).all()
+        return result
     
-    def get_one_by_id(self, id: int | str) -> Application | None:
-        found = None
-        
-        try:
-            with Session(engine) as session:
-                statement = select(Application).where(Application.id == int(id))
-                result = session.exec(statement)
-                for applicant in result:
-                    print(id, applicant)
-                    found = applicant
-        except ValueError:
-            httpCode(400, "Bad request. Application ID required to be parsable integer.")
-        
-        return found
+    def get_one_by_id(self, id: int) -> Application:
+        with Session(engine) as session:
+            statement = select(Application).where(Application.id == id)
+            result = session.exec(statement).one()
+        return result
     
-    def create_new(self) -> int | None:
-        # return cursor.execute("""
-        #     INSERT INTO applications (name, last_name, first_name, middle_name, birth_date, sex, height, unit_num, street_num, street_name, po_box, city, province, postal_code, completed)
-        #     VALUES('', '', '', '', '', '', '', '', '', '', '', '', '', '', '')
-        # """).lastrowid
-        return
+    def create_new(self, name: str) -> int:
+        application = Application(
+            app_name=name, last_name="", first_name="", middle_name="", birth_date="", sex="", height=0, address_street_unit="", address_street_num="", address_street_name="", address_po="", city="", province="", postal_code="", submitted=0
+        )
+        with Session(engine) as session:    
+            session.add(application)
+            session.commit()
+            session.refresh(application)
+            result = session.get(Application, application.id)
+        return result.id
     
-    def update_one_by_id(self):
-        # print("updated")
-        return
+    def update_one_application(self, cur: Application, new: AppForm, submit: bool = False) -> Application | bool | None:
+        with Session(engine) as session:
+            # Update each cur value on session to new values
+            cur.app_name = new.app_name
+            cur.last_name = new.last_name
+            cur.first_name = new.first_name
+            cur.middle_name = new.middle_name
+            cur.birth_date = new.birth_date
+            cur.sex = new.sex.upper()
+            cur.height = new.height
+            cur.address_street_unit = new.address_street_unit
+            cur.address_street_num = new.address_street_num
+            cur.address_street_name = new.address_street_name
+            cur.address_po = new.address_po.upper()
+            cur.city = new.city
+            cur.province = new.province.upper()
+            cur.postal_code = new.postal_code.upper()
+            # Only flag to change for final submit
+            if submit: cur.submitted = 1
+            
+            # **FUTURE TDO**: iterate update values in a loop / ignore update if old/new values are identical
+            
+            
+            session.add(cur)
+            session.commit()
+            session.refresh(cur)
+        return cur
     
-    def delete_one_by_id(self, id: str) -> int | None:
-        # before = len(cursor.execute("SELECT * FROM applications").fetchall())
-        # cursor.execute("DELETE FROM applications WHERE id = " + id)
-        # after = len(cursor.execute("SELECT * FROM applications").fetchall())
-        # return None if after >= before else int(id)
-        return
+    def delete_one_application(self, application: Application) -> any:
+        with Session(engine) as session:
+            session.delete(application)
+            session.commit()
+        return application
     
-    def geneate_db_and_sample_values() -> list[Application]:
+    # Create database, table, and populate sample values helpers
+    def generate_db_and_table() -> None:
         SQLModel.metadata.create_all(engine)
-        generated_apps = []
+    
+    def generate_sample_values() -> list[Application]:
+        generated_apps = list()
         for a in db_sample_values: # shortened 'application' to 'a'
             application = Application(
                 app_name=a["app_name"], last_name=a["last_name"], first_name=a["first_name"], middle_name=a["middle_name"], birth_date=a["birth_date"], sex=a["sex"], height=a["height"], address_street_unit=a["address_street_unit"], address_street_num=a["address_street_num"], address_street_name=a["address_street_name"], address_po=a["address_po"], city=a["city"], province=a["province"], postal_code=a["postal_code"], submitted=a["submitted"]
